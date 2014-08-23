5d145e5d7f50b84e09c0498ba90b48dc461dac4e
Ian Williams <iwillinc@gmail.com>
2014-08-23 18:40:23 -0400

    Installed lodash

--- config/config.js
+++ config/config.js
@@ -1,9 +1,15 @@
 'use strict';
 
 /**
+ * Module Dependencies
+ */
+var _ = require('lodash');
+
+
+
+/**
  * Module Exports
  */
- 
 module.exports = _.extend(
   require(__dirname + '/../config/env/all.js'),
   require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.js') || {},
