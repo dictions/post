58d2626984d4076f205783dcf97383b0c5225fbc
Ian Williams <iwillinc@gmail.com>
2014-09-05 00:23:32 -0400

    Aggregate Social Rank example - Installed Async

--- app/controllers/rank/social/index.js
+++ app/controllers/rank/social/index.js
@@ -3,12 +3,37 @@
 /**
  * Module Dependencies
  */
+      var async = require('async');
+          var Q = require('q');
 var stumbleupon = require('./stumbleupon');
    var facebook = require('./facebook');
 
+// var socialRank = function(url){
+
+//   var deferred = Q.defer();
+
+//   async.parallel({
+//     stumbleuponViews: function(callback){
+//       stumbleupon.getData(url).then(function(data){
+//         callback(null, data.views);
+//       });
+//     },
+//     facebookShares: function(callback){
+//       facebook.getData(url).then(function(data){
+//         callback(null, data.share_count);
+//       });
+//     }
+//   },
+//   function(err, results) {
+//     if (err) deferred.reject(null);
+//     deferred.resolve(results);
+//   });
+
+//   return deferred.promise;
+
+// };
 
 /**
- * Expose
+ * Expose socialRank()
  */
-exports.stumbleupon = stumbleupon;
-exports.facebook = facebook;
+//exports.socialRank = socialRank;
